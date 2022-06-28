8d1dcbd35f48a1e5741e1b55e96314d9d7bf2d18
Aleksandr-Szestak <szestak@hotmail.com>
2022-06-28 13:51:14 +0300

    init: start Reverse Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function reverse (n) {
-  
-}
+    let maxSt = 0;  
+    let a = Math.abs(n);
+    let c;
+    while (a>0){
+        c = a % 10;
+        a = (a-c) / 10;
+        maxSt++
+    }
+    
+    let r = 0;
+    let rez = 0;
+    a=Math.abs(n);
+    while (a>0){
+        c = a % 10;
+        a = (a-c) / 10;
+        r++;
+        rez += c * (10 ** (maxSt-r));
+    }
+    
+    //return n>=0 ? rez : -rez;
+    return rez;
+    }
