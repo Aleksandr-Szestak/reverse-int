e20cf878e709326a98c575b7dc93680e01cbba11
Aleksandr Szestak <szestak@hotmail.com>
2024-08-07 22:46:40 +0300

    refactor: improve code readability

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 module.exports = function reverse (n) {
+
     let a = Math.abs(n);
     let maxSt = Math.ceil(Math.log10(a));  
     let c = 0, r = 0, rez = 0;
 
-    while (a>0){
+    while (a > 0) {
         c = a % 10;
-        a = (a-c) / 10;
-        r++;
-        rez += c * (10 ** (maxSt-r));
+        a = (a - c) / 10;
+        r += 1;
+        rez += c * (10 ** (maxSt - r));
     }
     
     return rez;
