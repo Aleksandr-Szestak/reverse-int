62fa6ce14fe894ea7d096714b61645e50f722394
Aleksandr-Szestak <szestak@hotmail.com>
2025-09-04 09:50:35 +0300

    feat: add implementation of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  let a = Math.abs(n);
+  const maxSt = Math.ceil(Math.log10(a));
+  let c = 0;
+  let r = 0;
+  let rez = 0;
+  while (a > 0) {
+    c = a % 10;
+    a = (a - c) / 10;
+    r += 1;
+    rez += c * 10 ** (maxSt - r);
+  }
+  return rez;
 };
